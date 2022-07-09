6a37f93a87feb7775dc5d346fdc346b250288d9d
delphython <delphython@gmail.com>
2022-07-09 10:15:32 +0300

    add task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    // int inputNumber = int.Parse(inputLine);
+    // int outputNumber = inputNumber % 10;
+    
+    char outputNumber = inputLine[inputLine.Length - 1];
+
+    Console.WriteLine(outputNumber);
+}
