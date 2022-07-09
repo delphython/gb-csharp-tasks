8f6eb03065515de1d6e6872df344db0daf4d141a
delphython <delphython@gmail.com>
2022-07-09 09:00:29 +0300

    add task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    string[] dayOfWeek = {
+        "понедельник",
+        "вторник",
+        "среда",
+        "четверг",
+        "пятница",
+        "суббота",
+        "воскресенье"
+    };
+
+    Console.WriteLine(
+        "Номеру {0} соотвествует день недели: {1}.",
+        inputNumber,
+        dayOfWeek[inputNumber-1]
+    );
+}
