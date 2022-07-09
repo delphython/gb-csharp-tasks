d343d0db2474983b74f131432dc684254ecb38b1
delphython <delphython@gmail.com>
2022-07-09 09:41:06 +0300

    add task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int startNumber = 1;
+    string lineOutput = "0";
+
+    while (startNumber <= inputNumber)
+    {
+        lineOutput = startNumber*(-1) + ", " + lineOutput + ", " + startNumber;
+        startNumber++;
+    }
+
+    Console.WriteLine(lineOutput);
+}
