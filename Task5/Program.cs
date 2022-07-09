9026e708d2b7cec36161d5aed6a73672d97d992b
delphython <delphython@gmail.com>
2022-07-09 10:21:53 +0300

    add task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿string? inputFirstLine = Console.ReadLine();
+string? inputSecondLine = Console.ReadLine();
+
+if (inputFirstLine != null && inputSecondLine != null)
+{
+    int inputFirstNumber = int.Parse(inputFirstLine);
+    int inputSecondNumber = int.Parse(inputSecondLine);
+
+    Console.WriteLine(
+        "Максимальным из чисел {0} и {1} является число {2}",
+        inputFirstNumber,
+        inputSecondNumber,
+        Math.Max(inputFirstNumber, inputSecondNumber)
+    );
+}
