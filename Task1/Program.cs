b82b950bd383d68bafe89cb19e122b818b10705f
delphython <delphython@gmail.com>
2022-07-09 08:36:07 +0300

    add task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿string? inputFirstLine = Console.ReadLine();
+string? inputSecondLine = Console.ReadLine();
+
+if (inputFirstLine != null && inputSecondLine != null)
+{
+    int inputFirstNumber = int.Parse(inputFirstLine);
+    int inputSecondNumber = int.Parse(inputSecondLine);
+
+    if (inputSecondNumber == Math.Sqrt(inputFirstNumber))
+    {
+        Console.WriteLine(
+            "The first number {0} is the square of the second number {1}",
+            inputFirstNumber,
+            inputSecondNumber
+        );
+    }
+    else
+    {
+        Console.WriteLine(
+            "The first number {0} is not the square of the second number {1}",
+            inputFirstNumber,
+            inputSecondNumber
+        );
+    }
+}
