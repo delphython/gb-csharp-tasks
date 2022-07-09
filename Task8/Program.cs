a2c68f0db0168a6a6fa86d71068de823a918d97a
delphython <delphython@gmail.com>
2022-07-09 11:18:15 +0300

    add task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,24 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int start = 1;
+    int i = 0;
+    int arrayLenght = inputNumber/2;
+    int[] evenNumbersArray = new int[arrayLenght];
+    
+    while (start <= inputNumber)
+    {
+        if (start%2 == 0)
+        {
+            evenNumbersArray[i] = start;
+            i++;
+        }
+        start++;
+    }
+
+    string lineOutput = string.Join(", ", evenNumbersArray);
+
+    Console.WriteLine(lineOutput);
+}
