3b6a87e6aae961a0ebe0462e4db104c6b0648d62
delphython <delphython@gmail.com>
2022-07-09 10:33:10 +0300

    add task 7

---
+++ Task7/Program.cs
@@ -0,0 +1,8 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    Console.WriteLine("Число {0}" + ((inputNumber%2 == 0) ? " четное." : " нечетное."), inputNumber);
+}
