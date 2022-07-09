9241e9f8b23b21912ebc5f94692f8c64075eb87d
delphython <delphython@gmail.com>
2022-07-09 10:26:52 +0300

    add task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,22 @@
+﻿string? inputFirstLine = Console.ReadLine();
+string? inputSecondLine = Console.ReadLine();
+string? inputThirdLine = Console.ReadLine();
+
+if (
+    inputFirstLine != null && 
+    inputSecondLine != null && 
+    inputThirdLine != null
+)
+{
+    int inputFirstNumber = int.Parse(inputFirstLine);
+    int inputSecondNumber = int.Parse(inputSecondLine);
+    int inputThirdNumber = int.Parse(inputThirdLine);
+
+    Console.WriteLine(
+        "Максимальным из чисел {0}, {1} и {2} является число {3}",
+        inputFirstNumber,
+        inputSecondNumber,
+        inputThirdNumber,
+        Math.Max(inputFirstNumber, Math.Max(inputSecondNumber, inputThirdNumber))
+    );
+}
