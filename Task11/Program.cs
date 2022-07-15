5cf7cd016bf51708880ca6048b7de5860fb203d7
delphython <delphython@gmail.com>
2022-07-15 17:32:34 +0300

    add task 11

---
+++ Task11/Program.cs
@@ -0,0 +1,31 @@
+﻿//------------------------ Решение задачи 11 ------------------------------
+// Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если второе число некратно
+// первому, то программа выводит о статок от деления.
+//------------------------------------------------------------------------
+
+// Считываем данные с консоли
+string? inputFirstLine = Console.ReadLine();
+string? inputSecondLine = Console.ReadLine();
+
+if (inputFirstLine != null && inputSecondLine != null)
+{
+    try 
+    {
+        //Парсим первое число
+        int inputFirstNumber = int.Parse(inputFirstLine);
+        //Парсим второе число
+        int inputSecondNumber = int.Parse(inputSecondLine);
+        
+        // Проверяем, есть ли остаток от деления и выводим данные в консоль
+        Console.WriteLine("Число {0}" + (
+            (inputFirstNumber % inputSecondNumber == 0) ? " кратно {1}" : " не кратно {1}"
+        ), inputFirstNumber, inputSecondNumber);
+
+    }
+    catch
+    {
+        // Выводим сообщение об ошибке
+        Console.WriteLine("error!");
+    }
+}
