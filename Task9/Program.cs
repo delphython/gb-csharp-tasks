bea23ad99d2f6f3e69d9ff23dfb13d8cc3174833
delphython <delphython@gmail.com>
2022-07-15 16:11:27 +0300

    add second variant of function

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -3,8 +3,8 @@
 // и показывает наибольшую цифру числа.
 //------------------------------------------------------------------------
 
-// Объявляем метод getMaxDigitInNumber 
-void getMaxDigitInNumber()
+// Объявляем метод getMaxDigitInNumberVersion1 
+void getMaxDigitInNumberVersion1()
 {
     // Генерируем случайное число и переводим его в строку
     string number = new Random().Next(10, 100).ToString();
@@ -21,5 +21,29 @@ void getMaxDigitInNumber()
     );
 }
 
-// Вызываем метод getMaxDigitInNumber 
-getMaxDigitInNumber();
+// Объявляем метод getMaxDigitInNumberVersion2 
+void getMaxDigitInNumberVersion2()
+{
+    // Генерируем случайное число и переводим его в строку
+    string number = new Random().Next(10, 100).ToString();
+
+    // Объявляем список
+    List<char> digitsList = new List<char>();
+
+    // Добавляем элементы указанной коллекции в конец списка
+    digitsList.AddRange(number);
+
+    // Выводим в консоль информацию с максимальным значением
+    // цифры среди элементов списка
+    Console.WriteLine(
+        "Максимальной цифрой числа {0} является цифра {1}",
+        number,
+        digitsList.Max()
+    );
+}
+
+// Вызываем метод getMaxDigitInNumberVersion1 
+getMaxDigitInNumberVersion1();
+
+// Вызываем метод getMaxDigitInNumberVersion2 
+getMaxDigitInNumberVersion2();
