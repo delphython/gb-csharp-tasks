5667dd9552dd4ae807b7f5f43b0e88970dd08365
delphython <delphython@gmail.com>
2022-08-06 16:52:24 +0300

    add task 33

---
+++ Task33/Program.cs
@@ -0,0 +1,30 @@
+﻿//------------------------ Решение задачи 33 ------------------------------
+// Напишите программу замена элементов массива: положительные элементы 
+// замените на соответствующие отрицательные, и наоборот.
+//------------------------------------------------------------------------
+
+try 
+{
+    // Входные данные
+    int arrayLength = 5;
+    int[] firstArray = new int[] {10,-20,30,-40,50};
+    int[] secondArray = new int[arrayLength];
+    
+    // Замена знака на противоположный
+    for (int i=0; i<arrayLength; i++)
+    {
+        secondArray[i]=-firstArray[i];
+    }
+    
+    // Формируемс строки для вывода массивов
+    string lineOutputOne = string.Join(",", firstArray);
+    string lineOutputTwo = string.Join(",", secondArray);
+
+    // Выводим информацию
+    Console.WriteLine($"Был массиве [{lineOutputOne}], а стал [{lineOutputTwo}]");
+}
+catch (Exception e)
+{
+    // Выводим сообщение об ошибке
+    Console.WriteLine($"Что-то здесь не так: {e}");
+}
