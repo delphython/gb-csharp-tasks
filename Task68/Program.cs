1c1107dc6ee29f946d04bfeb854daedcf936c8a0
delphython <delphython@gmail.com>
2022-08-24 14:31:03 +0300

    add task 68

---
+++ Task68/Program.cs
@@ -0,0 +1,38 @@
+﻿//------------------------ Решение задачи 68 ------------------------------
+// Напишите программу, которая будет принимать на вход число и возвращать 
+// сумму его цифр.
+//------------------------------------------------------------------------
+
+// Метод SumNumberDigits, который считает сумму цифр числа
+int SumNumberDigits(int number)
+{
+    if(number==0) return 0;
+    return number % 10 + SumNumberDigits(number / 10); 
+}
+
+// Метод GetConsoleData для считывания данных с консоли
+void GetConsoleData(out int m)
+{
+    m = 0;
+    
+    Console.WriteLine("Введите число m");
+    Console.Write("m = ");
+    string? inputFirstLine = Console.ReadLine();
+    
+    if (inputFirstLine != null) m = int.Parse(inputFirstLine);
+}
+
+try 
+{
+    //Считываем данные с консоли
+    int m;
+    GetConsoleData(out m);
+
+    // Выводим информацию в консоль
+    Console.WriteLine($"Сумма цифр числа {m} = {SumNumberDigits(m)}");
+}
+catch (Exception e)
+{
+    // Выводим сообщение об ошибке
+    Console.WriteLine($"Что-то здесь не так: {e}");
+}
